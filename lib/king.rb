cc453cb3b817eb40ac8bdae79980717bc4a8380a
Robert Lee <RML7DU@gmail.com>
2020-07-30 11:29:18 -0600

    initial king class, add initialize, character code, and possiblemoves

---
+++ lib/king.rb
@@ -0,0 +1,31 @@
+class King
+    attr_accessor :x, :y, :player, :code
+
+    def initialize(x, y, player)
+        @x = x
+        @y = y
+        @player = player
+        @code = character_code(@player)
+    end
+
+    def character_code(player)
+        player % 2 == 0 ? "\u2654" : "\u265A" 
+    end
+
+    def possible_moves()
+        #diagnols
+        [@y + 1, @x + 1]
+        [@y + 1, @x - 1]
+        [@y - 1, @x - 1]
+        [@y - 1, @x + 1]
+
+        #horizontals
+        [@y, @x + 1]
+        [@y, @x - 1]
+
+        #verticals
+        [@y + 1, @x]
+        [@y - 1, @x]
+        
+    end
+end
