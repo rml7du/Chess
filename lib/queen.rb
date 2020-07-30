4b64609ace76c7c031ecd0c9e60caa8a18b570d0
Robert Lee <RML7DU@gmail.com>
2020-07-30 11:25:47 -0600

    initial queen class, add initialize, charachter code, possible moves

---
+++ lib/queen.rb
@@ -0,0 +1,84 @@
+class Queen
+    attr_accessor :x, :y, :player, :code
+
+    def initialize(x, y, player)
+        @x = x
+        @y = y
+        @player = player
+        @code = character_code(@player)
+    end
+
+    def character_code(player)
+        player % 2 == 0 ? "\u2655" : "\u265B" 
+    end
+
+    def possible_moves()
+        #diagnols
+        [@y + 1, @x + 1]
+        [@y + 2, @x + 2]
+        [@y + 3, @x + 3]
+        [@y + 4, @x + 4]
+        [@y + 5, @x + 5]
+        [@y + 6, @x + 6]
+        [@y + 7, @x + 7]
+
+        [@y + 1, @x - 1]
+        [@y + 2, @x - 2]
+        [@y + 3, @x - 3]
+        [@y + 4, @x - 4]
+        [@y + 5, @x - 5]
+        [@y + 6, @x - 6]
+        [@y + 7, @x - 7]
+
+        [@y - 1, @x - 1]
+        [@y - 2, @x - 2]
+        [@y - 3, @x - 3]
+        [@y - 4, @x - 4]
+        [@y - 5, @x - 5]
+        [@y - 6, @x - 6]
+        [@y - 7, @x - 7]
+
+        [@y - 1, @x + 1]
+        [@y - 2, @x + 2]
+        [@y - 3, @x + 3]
+        [@y - 4, @x + 4]
+        [@y - 5, @x + 5]
+        [@y - 6, @x + 6]
+        [@y - 7, @x + 7]
+
+        #horizontals
+        [@y, @x + 1]
+        [@y, @x + 2]
+        [@y, @x + 3]
+        [@y, @x + 4]
+        [@y, @x + 5]
+        [@y, @x + 6]
+        [@y, @x + 7]
+
+        [@y, @x - 1]
+        [@y, @x - 2]
+        [@y, @x - 3]
+        [@y, @x - 4]
+        [@y, @x - 5]
+        [@y, @x - 6]
+        [@y, @x - 7]
+
+        #verticals
+        [@y + 1, @x]
+        [@y + 2, @x]
+        [@y + 3, @x]
+        [@y + 4, @x]
+        [@y + 5, @x]
+        [@y + 6, @x]
+        [@y + 7, @x]
+
+        [@y - 1, @x]
+        [@y - 2, @x]
+        [@y - 3, @x]
+        [@y - 4, @x]
+        [@y - 5, @x]
+        [@y - 6, @x]
+        [@y - 7, @x]
+        
+    end
+end
