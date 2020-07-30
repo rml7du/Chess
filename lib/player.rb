aba5367ac2f5b2b9fbd35358c7ecf9eef8e2f206
Robert Lee <RML7DU@gmail.com>
2020-07-30 11:42:07 -0600

    inital player class, add initialzie and get name

---
+++ lib/player.rb
@@ -0,0 +1,15 @@
+class Player
+    attr_accessor :name, :player_number, :player_count
+    @@player_count = 0
+
+    def initialize()
+        @@player_count += 1
+        @name = enter_name()
+        @player_number = @@player_count
+    end
+
+    def enter_name()
+        puts "Hello player #{@@player_count}, please enter your name:"
+        return gets.chomp
+    end
+end
