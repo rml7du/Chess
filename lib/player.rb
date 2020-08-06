067008f2a2c313f598cbfeb358cb55e6076b7e51
Robert Lee <RML7DU@gmail.com>
2020-08-06 12:20:43 -0400

    added array to track remaining player pieces

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,12 @@
 class Player
-    attr_accessor :name, :player_number, :player_count
+    attr_accessor :name, :player_number, :player_count, :player_pieces
     @@player_count = 0
 
     def initialize()
         @@player_count += 1
         @name = enter_name()
         @player_number = @@player_count
+        @player_pieces = []
     end
 
     def enter_name()
